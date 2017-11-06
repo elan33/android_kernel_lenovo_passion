677f0695c1340fa209aca35b97fa8c8d4b805d4c
Alberto97 <albertop2197@gmail.com>
2017-11-06 15:26:19 +0100

    drivers: leds: sn3193: Remove Lenovo delay mess
    Change-Id: I7f3bf478226a234b536688d3057ffc1a95a351e0
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- drivers/leds/leds-sn3193.c
+++ drivers/leds/leds-sn3193.c
@@ -27,14 +27,11 @@
 #include <linux/of_device.h>
 #include <linux/regulator/consumer.h>
 
-#include <linux/delay.h>
 #include <linux/string.h>
 #include <linux/ctype.h>
 #include <linux/leds.h>
 #include <linux/workqueue.h>
 #include <linux/wakelock.h>
-#include <linux/slab.h>
-#include <linux/i2c.h>
 #include <linux/uaccess.h>
 #include <linux/fb.h>
 #include <linux/debugfs.h>
@@ -254,114 +251,6 @@ static struct i2c_driver sn3193_i2c_driver = {
 
 unsigned int	enable_ctrl;
 struct sn3193_leds_priv *g_sn3193_leds_data[2];
-///////////////triger timer
-static ssize_t led_delay_on_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-
-	return sprintf(buf, "%lu\n", led_cdev->blink_delay_on);
-}
-
-static ssize_t led_delay_on_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t size)
-{
-	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-	unsigned long state;
-	ssize_t ret = -EINVAL;
-
-	ret = kstrtoul(buf, 10, &state);
-	if (ret)
-		return ret;
-
-	led_blink_set(led_cdev, &state, &led_cdev->blink_delay_off);
-	led_cdev->blink_delay_on = state;
-
-	return size;
-}
-
-static ssize_t led_delay_off_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-
-	return sprintf(buf, "%lu\n", led_cdev->blink_delay_off);
-}
-
-static ssize_t led_delay_off_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t size)
-{
-	struct led_classdev *led_cdev = dev_get_drvdata(dev);
-	unsigned long state;
-	ssize_t ret = -EINVAL;
-
-	ret = kstrtoul(buf, 10, &state);
-	if (ret)
-		return ret;
-
-	led_blink_set(led_cdev, &led_cdev->blink_delay_on, &state);
-	led_cdev->blink_delay_off = state;
-
-	return size;
-}
-
-static DEVICE_ATTR(delay_on, 0644, led_delay_on_show, led_delay_on_store);
-static DEVICE_ATTR(delay_off, 0644, led_delay_off_show, led_delay_off_store);
-
-static void timer_trig_activate(struct led_classdev *led_cdev)
-{
-	int rc;
-
-	led_cdev->trigger_data = NULL;
-
-	rc = device_create_file(led_cdev->dev, &dev_attr_delay_on);
-	if (rc)
-		return;
-	rc = device_create_file(led_cdev->dev, &dev_attr_delay_off);
-	if (rc)
-		goto err_out_delayon;
-
-	led_blink_set(led_cdev, &led_cdev->blink_delay_on,
-		      &led_cdev->blink_delay_off);
-#ifdef  CONFIG_LEDS_TRIGGERS
-	led_cdev->activated = true;
-#endif
-
-	return;
-
-err_out_delayon:
-	device_remove_file(led_cdev->dev, &dev_attr_delay_on);
-}
-
-static void timer_trig_deactivate(struct led_classdev *led_cdev)
-{
-#ifdef CONFIG_LEDS_TRIGGERS
-	if (led_cdev->activated) {
-		device_remove_file(led_cdev->dev, &dev_attr_delay_on);
-		device_remove_file(led_cdev->dev, &dev_attr_delay_off);
-		led_cdev->activated = false;
-	}
-#endif
-	/* Stop blinking */
-	led_set_brightness(led_cdev, LED_OFF);
-}
-
-static struct led_trigger timer_led_trigger = {
-	.name     = "timer",
-	.activate = timer_trig_activate,
-	.deactivate = timer_trig_deactivate,
-};
-
-static int __init timer_trig_init(void)
-{
-	return led_trigger_register(&timer_led_trigger);
-}
-
-static void __exit timer_trig_exit(void)
-{
-	led_trigger_unregister(&timer_led_trigger);
-}
-
 
 static void sn3193_mdelay(u32 ms)
 {
@@ -464,28 +353,6 @@ void sn3193_off(void)
     sn3193_write_reg(0x1D, 0x00);
     sn3193_write_reg(0x07, 1);	
     sn3193_write_reg(0x00, 0x01);
-}
-
-
-static int  sn3193_blink_set(struct led_classdev *led_cdev,
-									unsigned long *delay_on,
-									unsigned long *delay_off)
-{
-
-
-	struct sn3193_leds_priv *led_data =
-		container_of(led_cdev, struct sn3193_leds_priv, cdev);
-
-//	LEDS_DEBUG("[LED]%s delay_on=0x%x, delay_off=0x%x\n", __func__,*delay_on,*delay_off);
-
-	if (*delay_on != led_data->delay_on || *delay_off != led_data->delay_off) {
-		led_data->delay_on = *delay_on;
-		led_data->delay_off = *delay_off;
-
-	
-	}
-	
-	return 0;
 }
 
 
@@ -707,7 +574,7 @@ static int  sn3193_leds_i2c_probe(struct i2c_client *client, const struct i2c_de
 	}
 	sn3193_i2c_cilent = client;
 
-	if(node){
+	if (node) {
 		enable_ctrl = of_get_named_gpio(node, "lenovo,en-gpio", 0);
 		if ((!gpio_is_valid(enable_ctrl))){
 			printk("[JX]!!ERROR!! gpio unvalid\n");
@@ -720,10 +587,15 @@ static int  sn3193_leds_i2c_probe(struct i2c_client *client, const struct i2c_de
 		if(node_name != NULL) g_sn3193_leds_data[0]->cdev.name = node_name;
 	
 		ret = led_classdev_register(&client->dev, &g_sn3193_leds_data[0]->cdev);
-		if (ret) goto err;
+		if (ret)
+			goto err;
+
 		sn3193_init();
-	}else printk("%s no node",__func__);
+	} else
+		printk("%s no node",__func__);
+
 	return ret;
+
 err:
 	i2c_del_driver(&sn3193_i2c_driver);
 	for (i = 1; i >=0; i--) {
@@ -774,7 +646,6 @@ static int __init sn3193_leds_init(void)
 	g_sn3193_leds_data[0]->cdev.name = LED_NAME;
 	g_sn3193_leds_data[0]->cdev.brightness_set = sn3193_led_set;
 	g_sn3193_leds_data[0]->cdev.max_brightness = 0xffffffff;
-	g_sn3193_leds_data[0]->cdev.blink_set = sn3193_blink_set;
 	INIT_WORK(&g_sn3193_leds_data[0]->work, sn3193_led_work);
 	g_sn3193_leds_data[0]->level = 0;
 
@@ -782,8 +653,7 @@ static int __init sn3193_leds_init(void)
 	{
 		printk("add i2c driver error %s\n",__func__);
 		goto err;
-	} 
-	timer_trig_init();
+	}
 	return 0;
 	
 err:
@@ -802,7 +672,6 @@ err:
 static void __exit sn3193_leds_exit(void)
 {
 	i2c_del_driver(&sn3193_i2c_driver);
-	timer_trig_exit();
 }
 
 module_param(debug_enable, int,0644);
